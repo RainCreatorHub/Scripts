d2d4670417a72f02d7b80c4c0204928c026478e4
RainCreatorHub <rain.hub.creator@gmail.com>
2025-07-25 21:35:54 -0300

    Create Npcs.lua

---
+++ Utils/VoxSeas/Modules/Npcs.lua
@@ -0,0 +1,30 @@
+local BanditList = {
+  "Bandit1", "Bandit2", "Bandit3", "Bandit4", "Bandit5",
+  "Bandit6", "Bandit7", "Bandit8", "Bandit9", "Bandit10",
+  "Bandit11", "Bandit12", "Bandit13", "Bandit14", "Bandit15",
+  "Bandit16", "Bandit17", "Bandit18", "Bandit19", "Bandit20",
+  "Bandit21", "Bandit22", "Bandit23", "Bandit24", "Bandit25",
+  "Bandit26", "Bandit27", "Bandit28", "Bandit29", "Bandit30",
+  "Bandit31", "Bandit32", "Bandit33", "Bandit34", "Bandit35",
+  "Bandit36", "Bandit37", "Bandit38", "Bandit39", "Bandit40",
+  "Bandit41", "Bandit42", "Bandit43", "Bandit44", "Bandit45",
+  "Bandit46", "Bandit47", "Bandit48", "Bandit49", "Bandit50",
+  "Bandit51", "Bandit52", "Bandit53", "Bandit54", "Bandit55",
+  "Bandit56", "Bandit57", "Bandit58", "Bandit59", "Bandit60",
+  "Bandit61", "Bandit62", "Bandit63", "Bandit64", "Bandit65",
+  "Bandit66", "Bandit67", "Bandit68", "Bandit69", "Bandit70",
+  "Bandit71", "Bandit72", "Bandit73", "Bandit74", "Bandit75",
+  "Bandit76", "Bandit77", "Bandit78", "Bandit79", "Bandit80",
+  "Bandit81", "Bandit82", "Bandit83", "Bandit84", "Bandit85",
+  "Bandit86", "Bandit87", "Bandit88", "Bandit89", "Bandit90",
+  "Bandit91", "Bandit92", "Bandit93", "Bandit94", "Bandit95",
+  "Bandit96", "Bandit97", "Bandit98", "Bandit99", "Bandit100"
+}
+
+local NPCS = {
+  {
+    Bandits = BanditList,
+    Level = 5,
+    Requer = 1
+  }
+}
